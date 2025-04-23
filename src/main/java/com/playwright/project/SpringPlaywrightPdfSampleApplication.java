79836733c301a94b45e658f22a8c58ba66d36d1b
whwp4151 <whwp4151@naver.com>
2025-04-23 13:58:33 +0900

    first commit

---
+++ src/main/java/com/playwright/project/SpringPlaywrightPdfSampleApplication.java
@@ -0,0 +1,13 @@
+package com.playwright.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlaywrightPdfSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlaywrightPdfSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playwright/project/SpringPlaywrightPdfSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playwright.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlaywrightPdfSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
